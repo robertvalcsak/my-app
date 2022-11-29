080fae9ee9909cda01fa4712aee1e41806ea83cb
Róbert Valcsák <robertvalcsak93@gmail.com>
2022-11-29 11:30:23 +0100

    destruct 2

--- src/components/Beer.js
+++ src/components/Beer.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-function Beer({beerData}) {
-    console.log({beerData})
+// function Beer({beerData}) {
+    function Beer(props) {
+        console.log({props})
+        
+        // const beerData = props.beerData
+        // const {beerData} = props
+        const {title, sub} = props.beerData
+
   return (
     <div>
-      <h1>{beerData.title}</h1>
-      <p>{beerData.sub}</p>
+      <h1>{title}</h1>
+      <p>{sub}</p>
     </div>
   )
 }
